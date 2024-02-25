21d4ddf65daf6d3dd97a08cbeb39a77813bedca9
jeonwoosun <janyjeon@naver.com>
2024-02-25 12:00:49 +0900

    00 강좌 소개

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
-  <Navbar />
-  <div class="container">
-      <router-view/>
-  </div>
-  
-  <Toast />
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import Toast from '@/components/Toast.vue';
-import Navbar from '@/components/Navbar.vue';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+  name: 'App',
   components: {
-    Toast,
-    Navbar
-  },
+    HelloWorld
+  }
 }
 </script>
 
-<style scoped>
-
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
